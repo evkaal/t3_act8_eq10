e369d2bf3fbbe1985df8ebdfed70d9520c606a09
AzaelJoestar <azael.h.uvera@gmail.com>
2026-07-14 08:29:27 -0600

    actu main

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// Esta es la línea clave: debe apuntar a tu App unificado
 import App from './app/App'; 
 import './index.css';
 
